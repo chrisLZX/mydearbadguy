67f9c2369115340069cc329d1ad07778c02a31b3
Chris Moya <cmoyasuoc@gmail.com>
2012-08-20 10:25:12 +0200

    Initial upload

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+//Replace XXX with your twitter application variables.
+define('CONSUMER_KEY', 'lmeapMsYwfZMzQ0d8Xa8Kg');
+define('CONSUMER_SECRET', 'deAxjTvyRiAOJyUqG3cTavo6VK4lW8xEYC2R8akrwI');
+define('OAUTH_CALLBACK', 'http://spainrevolution.com/mydearbadguy/process.php');
+?>
